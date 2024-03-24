845f27b37d37c209738af68af406b068f444bca2
Konstantin <lolkup705@gmail.com>
2024-03-24 10:42:55 +0900

    add faile Exercise_01

---
+++ Exercise_01/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 1: Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+*/
+
+System.Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 7 == 0 && num % 23 == 0)
+        {
+            Console.WriteLine("Число кратно и 7, и 23.");
+        }
+        else if (num % 7 == 0)
+        {
+            Console.WriteLine("Число кратно только 7.");
+        }
+        else if (num % 23 == 0)
+        {
+            Console.WriteLine("Число кратно только 23.");
+        }
+        else
+        {
+            Console.WriteLine("Число не кратно ни 7, ни 23.");
+        }
